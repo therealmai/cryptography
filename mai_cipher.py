0f466df1df38a58f88505390bb6e05509e3a9ddb
Jomar Leaño <69794504+therealmai@users.noreply.github.com>
2022-11-28 22:51:59 +0800

    done with python

--- mai_cipher.py
+++ mai_cipher.py
@@ -9,14 +9,14 @@ monoalpha_cipher = {'a': 'm', 'b': 'n', 'c': 'b', 'd': 'v', 'e': 'c', 'f': 'x','
 }
 
 def encrypt_with_monoalpha(message, monoalpha_cipher):
-   encrypted_message = []
-   for letter in message:
-      encrypted_message.append(monoalpha_cipher.get(letter, letter))
-   return ''.join(encrypted_message)
+    encrypted_message = []
+    for letter in message:
+        encrypted_message.append(monoalpha_cipher.get(letter, letter))
+    return ''.join(encrypted_message)
 
 
 def decrypt_with_monoalpha(encrypted_message, monoalpha_cipher):
-   return encrypt_with_monoalpha(
+    return encrypt_with_monoalpha(
       encrypted_message,
       inverse_monoalpha_cipher(monoalpha_cipher)
    )
@@ -29,20 +29,20 @@ def inverse_monoalpha_cipher(monoalpha_cipher):
 
 
 def random_monoalpha_cipher(pool = None):
-   if pool is None:
-      pool = ascii_letters + digits
-   original_pool = list(pool)
-   shuffled_pool = list(pool)
-   shuffle(shuffled_pool)
-   return dict(zip(original_pool, shuffled_pool))
+    if pool is None:
+        pool = ascii_letters + digits
+    original_pool = list(pool)
+    shuffled_pool = list(pool)
+    shuffle(shuffled_pool)
+    return dict(zip(original_pool, shuffled_pool))
 
 
 def generate_keys():
-    (publicKey, privateKey) = rsa.newkeys(1024)
-    with open('keys/publcKey.pem', 'wb') as p:
-        p.write(publicKey.save_pkcs1('PEM'))
-    with open('keys/privateKey.pem', 'wb') as p:
-        p.write(privateKey.save_pkcs1('PEM'))
+    (public_key, private_key) = rsa.newkeys(1024)
+    with open('keys/publcKey.pem', 'wb') as _p:
+        _p.write(public_key.save_pkcs1('PEM'))
+    with open('keys/privateKey.pem', 'wb') as _p:
+        _p.write(private_key.save_pkcs1('PEM'))
     print("\nKeys are generated in ./keys/\n")
 
 
@@ -53,6 +53,10 @@ def load_keys():
         private_key = rsa.PrivateKey.load_pkcs1(p.read())
     cipher_key:str = random_key()
     mono_cipher = random_monoalpha_cipher()
+    print(f"AES_Public_Key: {public_key}\n")
+    print(f"AES_Private_Key: {private_key}\n")
+    print(f"Cipher Key: {cipher_key}\n")
+    print(f"MonoAlphabetic Key: {mono_cipher}\n")
     return public_key, private_key, cipher_key, mono_cipher
 
 
@@ -77,9 +81,13 @@ def encrypted(rsa_key, vignere_key, mono_key):
     filename = input("Enter Filename:")
 
     cipher_text = encrypt(message, vignere_key)
+    print(f"Vignere Cipher: {cipher_text}\n")
     cipher_text = atbash(cipher_text)
+    print(f"Atbash Cipher: {cipher_text}")
     cipher_text = encrypt_with_monoalpha(cipher_text, mono_key)
+    print(f"Monoalphabetic Cipher: {cipher_text}")
     cipher_text = rsa.encrypt(cipher_text.encode('ascii'), rsa_key)
+    print(f"RSA Cipher: {cipher_text}")
 
     # Open File and Write Ciphertext into binary File
     _fp = open(filename, 'wb')
@@ -88,7 +96,7 @@ def encrypted(rsa_key, vignere_key, mono_key):
         print(f'Plain Text: {message}')
         print(f'Mai Cipher: {cipher_text}')
     else:
-        print("Error in Writing")
+        print("Error in Writing File")
     _fp.close()
 
 
@@ -100,14 +108,19 @@ def decrypted(rsa_key, vignere_key, mono_key):
     _fp = open(filename, 'rb')
     if _fp is not None :
         encrypted_text = _fp.read()
+        print(f"Encrypted Text: {encrypted_text}\n")
         decrypted_val = rsa.decrypt(encrypted_text, rsa_key).decode('ascii')
+        print(f"Decrypted RSA: {decrypted_val}\n")
         decrypted_val = decrypt_with_monoalpha(decrypted_val, mono_key)
+        print(f"Decrypted MonoAlpha: {decrypted_val}\n")
         decrypted_val = atbash(decrypted_val)
+        print(f"Decrypted Atbash: {decrypted_val}\n")
         decrypted_val = decrypt(decrypted_val, vignere_key)
+        print(f"Decrypted Vignere: {decrypted_val}\n")
         print(f'Encrypted Text: {encrypted_text}')
         print(f'Mai Cipher: {decrypted_val}')
     else :
-        print("Error in Reading")
+        print("Error in Reading File")
     _fp.close()
 
 
