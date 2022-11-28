b04cb1925451dd975aaacc6f174481d937db8382
Jomar Leaño <69794504+therealmai@users.noreply.github.com>
2022-11-28 21:32:45 +0800

    done with 3106

--- mai_cipher.py
+++ mai_cipher.py
@@ -1,6 +1,40 @@
 import rsa
 from vigenere import encrypt, decrypt, random_key
+from string import ascii_letters, digits
+from random import shuffle
 
+monoalpha_cipher = {'a': 'm', 'b': 'n', 'c': 'b', 'd': 'v', 'e': 'c', 'f': 'x','g': 'z', 'h': 'a', 'i': 's', 'j': 'd', 'k': 'f',
+                    'l': 'g', 'm': 'h', 'n': 'j', 'o': 'k', 'p': 'l', 'q': 'p', 'r': 'o', 's': 'i', 't': 'u', 'u': 'y', 'v': 't', 'w': 'r', 'x': 'e', 'y': 'w', 
+                    'z': 'q', ' ': ' ',
+}
+
+def encrypt_with_monoalpha(message, monoalpha_cipher):
+   encrypted_message = []
+   for letter in message:
+      encrypted_message.append(monoalpha_cipher.get(letter, letter))
+   return ''.join(encrypted_message)
+
+
+def decrypt_with_monoalpha(encrypted_message, monoalpha_cipher):
+   return encrypt_with_monoalpha(
+      encrypted_message,
+      inverse_monoalpha_cipher(monoalpha_cipher)
+   )
+
+def inverse_monoalpha_cipher(monoalpha_cipher):
+   inverse_monoalpha = {}
+   for key, value in monoalpha_cipher.items():
+      inverse_monoalpha[value] = key
+   return inverse_monoalpha
+
+
+def random_monoalpha_cipher(pool = None):
+   if pool is None:
+      pool = ascii_letters + digits
+   original_pool = list(pool)
+   shuffled_pool = list(pool)
+   shuffle(shuffled_pool)
+   return dict(zip(original_pool, shuffled_pool))
 
 
 def generate_keys():
@@ -12,18 +46,14 @@ def generate_keys():
     print("\nKeys are generated in ./keys/\n")
 
 
-
-
 def load_keys():
     with open('keys/publcKey.pem', 'rb') as p:
         public_key = rsa.PublicKey.load_pkcs1(p.read())
     with open('keys/privateKey.pem', 'rb') as p:
         private_key = rsa.PrivateKey.load_pkcs1(p.read())
     cipher_key:str = random_key()
-    print(f"Private Key: {private_key}\n")
-    print(f"Public Key: {public_key}\n")
-    print(f"Cipher Key: {cipher_key}\n")
-    return public_key, private_key, cipher_key
+    mono_cipher = random_monoalpha_cipher()
+    return public_key, private_key, cipher_key, mono_cipher
 
 
 def atbash(plain):
@@ -39,58 +69,70 @@ def atbash(plain):
             crypt = crypt + chr ( jval )
         else:
             crypt = crypt + plain[i]
-
     return crypt
 
 
-
-def encrypted(public_key, vignere_key,private_key):
+def encrypted(rsa_key, vignere_key, mono_key):
     message = input("Enter the message to be encrypted: ")
-    # message = rsa.encrypt(message.encode('ascii'), public_key)
-    # print(f"{message}\n")
+    filename = input("Enter Filename:")
 
-    message = encrypt(message, vignere_key) # with random_key function, cipher may vary everytime you run a program.
-    print(f"{message}")
-    message = rsa.encrypt(message.encode('ascii'), public_key)
-    print(f"{message}\n")
-    decrypted_val = rsa.decrypt(message, public_key).decode('ascii')
-    decrypted_val = decrypt(decrypted_val, vignere_key)
-    print(f"{decrypted_val}")
-    # cipher =decrypt(message, vignere_key)   
-    # print(f"{cipher}")
-    # return message
+    cipher_text = encrypt(message, vignere_key)
+    cipher_text = atbash(cipher_text)
+    cipher_text = encrypt_with_monoalpha(cipher_text, mono_key)
+    cipher_text = rsa.encrypt(cipher_text.encode('ascii'), rsa_key)
 
-
-
-def decrypted(ciphertext, key):
-    try:
-        cipher = decrypt(cipher, vignere_key)
-        return rsa.decrypt(ciphertext, key).decode('ascii')
-    except:
-        return False
+    # Open File and Write Ciphertext into binary File
+    _fp = open(filename, 'wb')
+    if(_fp.writable() and _fp is not None) :
+        _fp.write(cipher_text)
+        print(f'Plain Text: {message}')
+        print(f'Mai Cipher: {cipher_text}')
+    else:
+        print("Error in Writing")
+    _fp.close()
 
 
 
+def decrypted(rsa_key, vignere_key, mono_key):
+    filename = input("Enter Filename: ")
+
+    #Open File and Read Ciphertext and Decrypt
+    _fp = open(filename, 'rb')
+    if _fp is not None :
+        encrypted_text = _fp.read()
+        decrypted_val = rsa.decrypt(encrypted_text, rsa_key).decode('ascii')
+        decrypted_val = decrypt_with_monoalpha(decrypted_val, mono_key)
+        decrypted_val = atbash(decrypted_val)
+        decrypted_val = decrypt(decrypted_val, vignere_key)
+        print(f'Encrypted Text: {encrypted_text}')
+        print(f'Mai Cipher: {decrypted_val}')
+    else :
+        print("Error in Reading")
+    _fp.close()
 
 
 if __name__ == '__main__':
-    # CHOICE = 0
-    # while CHOICE != 4:
-    #     print("----------Mai Cipher----------")
-    #     print("Encrypt______________________1")
-    #     print("Decrypt______________________2")
-    #     print("Generate Keys________________3")
-    #     print("Exit_________________________5")
-    #     CHOICE = input("Enter your choice: ")
+    PUBLIC_KEY = ''
+    VIGNERE_KEY = ''
+    PRIVATE_KEY = ''
+    MONO_CIPHER_KEY = ''
+    CHOICE = 0
+    while CHOICE != 4:
+        print("----------Mai Cipher----------")
+        print("Encrypt______________________1")
+        print("Decrypt______________________2")
+        print("Generate Keys________________3")
+        print("Exit_________________________4")
+        CHOICE = input("Enter your choice: ")
 
-    #     if CHOICE  == '1':
-    #          encrypted(public_key,vignere_key,private_key)
-    #     elif CHOICE == '2':
-    #         print("Input 2")
-    #     elif CHOICE == '3':
-    #         generate_keys()
-    #         (private_key, public_key, vignere_key) = load_keys()
-    #     elif CHOICE == '4':
-    #         break
-    #     else:
-    #         print("Wrong Input")
+        if CHOICE  == '1':
+            encrypted(PUBLIC_KEY, VIGNERE_KEY, MONO_CIPHER_KEY)
+        elif CHOICE == '2':
+            decrypted(PUBLIC_KEY, VIGNERE_KEY, MONO_CIPHER_KEY)
+        elif CHOICE == '3':
+            generate_keys()
+            (PRIVATE_KEY, PUBLIC_KEY, VIGNERE_KEY,MONO_CIPHER_KEY) = load_keys()
+        elif CHOICE == '4':
+            break
+        else:
+            print("Wrong Input")
